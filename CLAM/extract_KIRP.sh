46a721f8fba5d7f743db644dcb66179b7766463a
Tuner12 <88604476+Tuner12@users.noreply.github.com>
2025-07-21 18:45:29 -0700

    Create extract_KIRP.sh

---
+++ CLAM/extract_KIRP.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# 设置模型列表
+MODELS=("uni_v2"  "phikon_v2" "gigapath" "hoptimus1" virchow2)
+# "gigapath"  "phikon_v2" "gigapath" "hoptimus1"
+# MODELS=("hoptimus1")
+# 设置数据 CSV 文件偏移量（如果在另一个服务器上，可设为 8）
+OFFSET=0
+BATCH_SIZE=64
+JSON_MAP="../survival_analysis/TCGA_KIRP_directory/svs_path.json"
+# 遍历模型
+GPU_IDS=(4 5 6 7)
+for model in "${MODELS[@]}"; do
+  echo "==== Processing model: $model ===="
+  # 设置每个模型的 batch size（hoptimus0 用较小值）
+  # if [ "$model" = "hoptimus1" ]; then
+  #   BATCH_SIZE=256
+  # else
+  #   BATCH_SIZE=512
+  # fi
+  # 启动 8 张卡并行提取同一模型的不同数据部分
+  for i in {0..3}; do
+    csv_index=$((OFFSET + i))
+    gpu_id=${GPU_IDS[i]}
+    # feat_dir="/home/leiwenhui/18data2/leiwenhui/Data/Extracted_Feature/TCGA_KIRP_multi_features/${model}_features/merged_pt_files"
+    feat_dir="/data2/leiwenhui/Data/Extracted_Feature/TCGA_KIRP_multi_features/${model}_features/merged_pt_files"
+    mkdir -p "$feat_dir"
+    CUDA_VISIBLE_DEVICES=$gpu_id python extract_multi_features.py \
+      --model_name $model \
+      --data_h5_dir ../survival_analysis/TCGA_KIRP_PATCH_DIR40 \
+      --data_slide_dir ../survival_analysis/TCGA_KIRP_directory \
+      --csv_path ../survival_analysis/TCGA_KIRP_PATCH_DIR40/process_list_autogen_part${csv_index}.csv \
+      --feat_dir "$feat_dir" \
+      --batch_size $BATCH_SIZE \
+      --slide_ext .svs \
+      --json_map "$JSON_MAP" \
+      > ../survival_analysis/logs/KIRP/${model}_gpu${csv_index}.txt 2>&1 &
+  done
+
+  # 等待当前模型的 8 个子进程全部结束
+  wait
+  echo "==== Completed model: $model ===="
+done
